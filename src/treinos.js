04a02388729c5b58b2cf5f508ffa72552067efac
Felipe Novello <felipenovello2006@hotmail.com>
2026-08-27 16:13:59 -0300

    laços for volume total dos treinos

--- src/treinos.js
+++ src/treinos.js
@@ -1,26 +1,29 @@
 
 let treinos = [
     {
-    exercicio: "supino",
-    carga: 26,
-    repeticoes: 12,
-    series: 4
+        exercicio: "supino",
+        carga: 26,
+        repeticoes: 12,
+        series: 4
     },
     {   
-    exercicio: "agachamento",
-    carga: 80,
-    repeticoes: 10,
-    series: 5
+        exercicio: "agachamento",
+        carga: 80,
+        repeticoes: 10,
+        series: 5
     },
     {
-    exercicio: "remada curvada",
-    carga: 50,
-    repeticoes: 11,
-    series: 4
+        exercicio: "remada curvada",
+        carga: 50,
+        repeticoes: 11,
+        series: 4
     }  
 ]
 
-console.log(treinos[0].carga)
-console.log(treinos[1])
-console.log(treinos[2].exercicio)
-console.log(treinos.length)
+let total = 0
+
+for (let treino of treinos) {
+    let volume = treino.carga *treino.series * treino.repeticoes
+    total += volume
+    console.log("Volume total: ", total)
+}
