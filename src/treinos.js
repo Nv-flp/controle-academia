eca50b65b1125d67482d56e57de974d75bc2b8d2
Felipe Novello <felipenovello2006@hotmail.com>
2026-08-27 13:54:14 -0300

    volume total de treino

--- src/treinos.js
+++ src/treinos.js
@@ -2,8 +2,10 @@ let exercicio = "supino"
 let carga = 26
 let repeticoes = 12
 let series = 4
+let volume = series * repeticoes * carga
 
 console.log(exercicio)
 console.log(carga)
 console.log(repeticoes)
 console.log(series)
+console.log("volume total:", volume)
