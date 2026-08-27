275e93cb7317ed3fee64204af15727ea3f063daf
Felipe Novello <felipenovello2006@hotmail.com>
2026-08-27 13:36:32 -0300

    Variaveis da forma correta

--- src/treinos.js
+++ src/treinos.js
@@ -1,4 +1,9 @@
 let exercicio = "supino"
-let carga = "26"
-let repeticoes = "12"
-let series = "4"
+let carga = 26
+let repeticoes = 12
+let series = 4
+
+console.log(exercicio)
+console.log(carga)
+console.log(repeticoes)
+console.log(series)
