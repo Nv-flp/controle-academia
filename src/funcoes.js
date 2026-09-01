cc6e6a58bd5440fe17ec6a2a2fedd2fd33592299
Felipe Novello <felipenovello2006@hotmail.com>
2026-09-01 09:28:11 -0300

    cria funcoes de carregar e calcular volume

--- src/funcoes.js
+++ src/funcoes.js
@@ -1,10 +1,16 @@
 const fs = require ("fs")
+
 function carregarTreinos () {
     const texto = fs.readFileSync("treinos.json", "utf-8")
     return JSON.parse(texto)
 }
 
-
+function calcularVolume (treino) {
+    return treino.carga * treino.series * treino.repeticoes
+}
 
 const treinos = carregarTreinos()
-console.log(treinos)
+
+for (let treino of treinos) {
+    console.log(treino.exercicio, calcularVolume(treino))
+}
