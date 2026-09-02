22eaadf98b2bdfbdee055e38fc87a77e4a3a39ba
Felipe Novello <felipenovello2006@hotmail.com>
2026-09-02 10:15:29 -0300

    CRUD completo via API com Express

--- src/servidor.js
+++ src/servidor.js
@@ -1,7 +1,10 @@
 const express = require("express")
-const { carregarTreinos } =  require("./funcoes")
+const { carregarTreinos, salvarTreinos } =  require("./funcoes")
 
 const app = express()
+
+app.use(express.json())
+
 
 app.get("/treinos", function (req, res) {
     const treinos = carregarTreinos()
@@ -20,6 +23,61 @@ app.get("/treinos/:id", function(req, res){
     }
 
     res.status(404).json({erro: "treino nao encontrado0" })
+})
+
+app.post("/treinos", function (req, res){
+    const treinos = carregarTreinos()
+    const novo = req.body
+
+    let maiorId = 0 
+
+    for (let treino of treinos) {
+        if (treino.id > maiorId) {
+            maiorId = treino.id
+        }
+    }
+
+    novo.id = maiorId + 1
+
+    treinos.push(novo)
+    salvarTreinos(treinos)
+
+    res.status(201).json(novo)
+})
+
+app.put ("/treinos/:id", function (req, res){
+    const treinos = carregarTreinos()
+    const id = Number(req.params.id)
+
+    for (let treino of treinos) {
+        if (treino.id === id) {
+         treino.carga = req.body.carga
+         salvarTreinos(treinos)
+         return res.json(treino)
+        }
+    }
+
+    res.status(404).json({erro: "treino nao encontrado"})
+})
+
+app.delete("/treinos/:id", function(req, res){
+    const treinos = carregarTreinos()
+    const id = Number(req.params.id)
+
+    const listaNova = []
+
+for (let treino of treinos) {
+  if (treino.id !== id) {
+    listaNova.push(treino)
+  }
+}
+
+if (listaNova.length === treinos.length) {
+  return res.status(404).json({ erro: "treino nao encontrado" })
+}
+
+   salvarTreinos(listaNova)
+res.json({ mensagem: "removido" })
 })
 
 app.listen(3000, function () {
